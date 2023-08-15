34d94e5a1ffde42a762c83b4cd1d67e21fac25f9
Mahesh M <MaheshM@keysoftwareinc.com>
2023-08-15 14:56:33 +0530

    navbaractivestatusfixed

--- PatientImport/PatientImport/src/app/ReusableComponents/nav-bar/nav-bar.component.ts
+++ PatientImport/PatientImport/src/app/ReusableComponents/nav-bar/nav-bar.component.ts
@@ -8,7 +8,5 @@ import { Router } from '@angular/router';
 })
 export class NavBarComponent {
   constructor(private router: Router) {}
-  isActive(route: string): boolean {
-    return this.router.isActive(route, false);
-}
+ 
 }
